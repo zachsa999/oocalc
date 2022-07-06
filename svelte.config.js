315b474d97bbed366bf6860043c11f9856d94c46
zachsa999 <56491639+zachsa999@users.noreply.github.com>
2022-07-06 17:31:57 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -7,10 +7,6 @@ const config = {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
 		}),
 		prerender: {
 			// This can be false if you're using a fallback (i.e. SPA mode)
