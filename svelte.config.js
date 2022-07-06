9c18ad05e025180ed11eab2681d073eaa0595857
zachsa999 <56491639+zachsa999@users.noreply.github.com>
2022-07-06 16:34:29 -0500

    static site
    add static deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	}
 };
 
