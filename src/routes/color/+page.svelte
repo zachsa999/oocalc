e1ba19398688807f81fbe4684a53046b17023a58
zachsa999 <zach@zachw.ca>
2022-11-01 10:41:38 -0500

    update npm and add static fallback and prerender options

--- src/routes/color/+page.svelte
+++ src/routes/color/+page.svelte
@@ -1,9 +1,8 @@
 <script>
-	/** @type {import('./$types').PageData} */
-	export let data;
+	let color = Math.floor(Math.random() * 16777215).toString(16);
 </script>
 
-<form method="POST" action="?textColorInput">
-	<input type="text" name="color" id="colorText" />
-	<input name="color" type="color" />
+<form>
+	<input type="text" name="color" id="colorText" value={color} />
+	<input name="color" type="color" value={color} />
 </form>
--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,10 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
+			fallback: '200.html'
 		}),
+		prerender: { entries: [] }
+
 	}
 };
 
