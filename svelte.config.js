0c6643f5fd9559348414468ff271a504766214e0
zachsa999 <zach@zachw.ca>
2022-11-03 08:37:12 -0500

    remove mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,7 @@
-import { mdsvex } from 'mdsvex';
-import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', ...mdsvexConfig.extensions],
 
 	kit: {
 		adapter: adapter({
@@ -13,7 +10,6 @@ const config = {
 		prerender: { entries: [] }
 	},
 
-	preprocess: [mdsvex(mdsvexConfig)]
 };
 
 export default config;
