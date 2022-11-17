ba5538f65b8201b48e718fad91485bf93f38b5fd
zachsa999 <zach@zachw.ca>
2022-11-17 15:58:11 -0600

    netlify

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,13 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 import { setupSupabaseHelpers } from '@supabase/auth-helpers-sveltekit'
 import { dev } from '$app/environment'
-import { env } from '$env/static/public'
+import { env } from '$env/dynamic/public'
 // or use the static env
 // import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
 export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY, {
-    persistSession: false,
-    autoRefreshToken: false,
+    persistSession: true,
+    autoRefreshToken: true,
 })
 
 setupSupabaseHelpers({
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 // import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,11 +7,13 @@ const config = {
 	kit: {
 		// adapter: adapter()
 		adapter: adapter({
-			fallback: '200.html',
-			pages: 'build',
-			assets: 'build',
-			precompress: false,
-			strict: true
+			// fallback: '200.html',
+			// pages: 'build',
+			// assets: 'build',
+			// precompress: false,
+			// strict: true
+			edge: false,
+			split: true
 		}),
 	},
 
